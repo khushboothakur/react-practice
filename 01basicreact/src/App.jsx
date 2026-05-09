f20de6f4648b8f3f57017f756d08b43cbb0fb775
Spotify Clone <khushbuthakur272006@gmail.com>
2026-05-09 23:41:32 +0530

    initial commit

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Chai from './chai.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Chai/>
+      <h1>hii</h1>
+    </> 
+  )
+}
+
+export default App
---
+++ 01basicreact/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    'click me to visit google'
+);
+
+createRoot(document.getElementById('root')).render(
+    reactElement
+)
